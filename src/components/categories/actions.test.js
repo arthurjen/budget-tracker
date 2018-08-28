7f878f1b6f0a5a1b6f3aee0c7c8269fbedf0ceed
Arthur Jen <arthurljen@gmail.com>
2018-08-28 12:48:19 -0700

    updated snapshots

--- src/components/categories/actions.test.js
+++ src/components/categories/actions.test.js
@@ -1,46 +1,46 @@
-// import { add, load, update, remove } from './actions';
-// import { CATEGORY_ADD, CATEGORY_LOAD, CATEGORY_UPDATE, CATEGORY_REMOVE } from './reducers';
+import { add, load, update, remove } from './actions';
+import { CATEGORY_ADD, CATEGORY_LOAD, CATEGORY_UPDATE, CATEGORY_REMOVE } from './reducers';
 
-// describe('categories actions', () => {
-//   it('adds a category', () => {
-//     const category = {
-//       name: 'Test Category',
-//       budget: 666,
-//     };
-//     const action = add(category);
-//     expect(action).toEqual({
-//       type: CATEGORY_ADD,
-//       payload: category
-//     });
-//     expect(action.payload.id).toBeDefined();
-//     expect(action.payload.timestamp).toBeDefined();
-//   });
+describe.skip('categories actions', () => {
+  it('adds a category', () => {
+    const category = {
+      name: 'Test Category',
+      budget: 666,
+    };
+    const action = add(category);
+    expect(action).toEqual({
+      type: CATEGORY_ADD,
+      payload: category
+    });
+    expect(action.payload.id).toBeDefined();
+    expect(action.payload.timestamp).toBeDefined();
+  });
 
-//   it('loads categories', () => {
-//     const action = load();
-//     expect(action).toEqual({
-//       type: CATEGORY_LOAD,
-//       payload: data
-//     });
-//   });
+  it('loads categories', () => {
+    const action = load();
+    expect(action).toEqual({
+      type: CATEGORY_LOAD,
+      payload: data
+    });
+  });
 
-//   it('updates a category', () => {
-//     const category = {
-//       name: 'Test Category',
-//       budget: 20,
-//     };
-//     const action = update(category);
-//     expect(action).toEqual({
-//       type: CATEGORY_UPDATE,
-//       payload: category
-//     });
-//   });
+  it('updates a category', () => {
+    const category = {
+      name: 'Test Category',
+      budget: 20,
+    };
+    const action = update(category);
+    expect(action).toEqual({
+      type: CATEGORY_UPDATE,
+      payload: category
+    });
+  });
 
-//   it('removes a category', () => {
-//     const action = remove('5');
-//     expect(action).toEqual({
-//       type: CATEGORY_REMOVE,
-//       payload: '5'
-//     });
-//   });
-// });
+  it('removes a category', () => {
+    const action = remove('5');
+    expect(action).toEqual({
+      type: CATEGORY_REMOVE,
+      payload: '5'
+    });
+  });
+});
